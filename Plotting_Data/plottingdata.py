780c3a59216f82e149e485afd3165626d6f57a18
SvetlanaPismenchuk <spismenchuk3085@floridapoly.edu>
2026-02-17 03:15:15 +0000

    Project

---
+++ Plotting_Data/plottingdata.py
@@ -0,0 +1,35 @@
+import Matplotlib.pyplot as plt
+
+def read_data(infile, xfield, yfield):
+     x = []
+     y = []
+     
+     with open(filename, 'r') as infile:
+        header = infile.readline().strip().split(',') #reads first line, removes newline/spaces, turns into list
+
+        x_index = header.index(xfield)
+        y_index = header.index(yfield)
+         #finds where a column name appears in the header row
+        
+        
+         for line in infile:
+            words = line.strip().split(',')
+            
+            x.append(float(words[x_index]))
+            y.append(float(words[y_index]))
+            
+    return x, y
+    
+
+
+def plot_data (infile, xfield, yfield):
+    x, y = read_data(infile, xfield, yfield) #calls function and stores the results #sends back two lists
+
+    plt.scatter(x,y) #makes a scatter plot x-horizontal y-vertical
+    plt.xlabel(xfield) #labels the x-axis
+    plt.ylabel(yfield) #labels the y-axis
+    plt.show() #displays graph
+
+plot_data("auto-mpg[1].csv", "mpg", "displacement")
+input()
+
